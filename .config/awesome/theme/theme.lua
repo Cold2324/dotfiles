e677c39c236ca56d549cf6392d919e3d33671189
Gonzalo Cabada <cabadagonzalo17@gmail.com>
2022-12-30 11:16:11 -0500

    awesome fix picom autostart and change gap from dpi(2) to dpi(3)

--- .config/awesome/theme/theme.lua
+++ .config/awesome/theme/theme.lua
@@ -240,7 +240,7 @@ theme.layoutlist_shape_selected = helpers.ui.rrect(theme.border_radius)
 theme.layoutlist_bg_selected = theme.widget_bg
 
 --- Gaps
-theme.useless_gap = dpi(2)
+theme.useless_gap = dpi(3)
 
 --- Systray
 theme.systray_icon_size = dpi(20)
